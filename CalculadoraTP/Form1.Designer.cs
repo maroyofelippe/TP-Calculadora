7274a7b95b3a4d446df5c76ae3c4a865b7679ccb
Marco Royo <marco.felippe@etec.sp.gov.br>
2019-10-12 00:47:01 -0300

    {FEAT_INIT}

---
+++ CalculadoraTP/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace CalculadoraTP
+{
+    partial class frmCalculadora
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCalculadora));
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnEleva = new System.Windows.Forms.Button();
+            this.btnConcatenar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnRaiz = new System.Windows.Forms.Button();
+            this.btnDividit = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnSubtrair = new System.Windows.Forms.Button();
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtN1
+            // 
+            this.txtN1.Location = new System.Drawing.Point(12, 53);
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(75, 20);
+            this.txtN1.TabIndex = 0;
+            // 
+            // txtN2
+            // 
+            this.txtN2.Location = new System.Drawing.Point(97, 55);
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(73, 20);
+            this.txtN2.TabIndex = 1;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Enabled = false;
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(12, 12);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(158, 29);
+            this.txtResultado.TabIndex = 2;
+            this.txtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnEleva
+            // 
+            this.btnEleva.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEleva.Location = new System.Drawing.Point(52, 119);
+            this.btnEleva.Name = "btnEleva";
+            this.btnEleva.Size = new System.Drawing.Size(35, 33);
+            this.btnEleva.TabIndex = 8;
+            this.btnEleva.Text = "Xy\r\n";
+            this.btnEleva.UseVisualStyleBackColor = true;
+            this.btnEleva.Click += new System.EventHandler(this.btnEleva_Click);
+            // 
+            // btnConcatenar
+            // 
+            this.btnConcatenar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConcatenar.Location = new System.Drawing.Point(12, 158);
+            this.btnConcatenar.Name = "btnConcatenar";
+            this.btnConcatenar.Size = new System.Drawing.Size(158, 23);
+            this.btnConcatenar.TabIndex = 9;
+            this.btnConcatenar.Text = "Concatena";
+            this.btnConcatenar.UseVisualStyleBackColor = true;
+            this.btnConcatenar.Click += new System.EventHandler(this.btnConcatenar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(97, 119);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(73, 33);
+            this.btnLimpar.TabIndex = 10;
+            this.btnLimpar.Text = "LIMPAR";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnRaiz
+            // 
+            this.btnRaiz.BackgroundImage = global::CalculadoraTP.Properties.Resources.Square_Root2;
+            this.btnRaiz.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRaiz.Location = new System.Drawing.Point(12, 119);
+            this.btnRaiz.Name = "btnRaiz";
+            this.btnRaiz.Size = new System.Drawing.Size(34, 33);
+            this.btnRaiz.TabIndex = 7;
+            this.btnRaiz.UseVisualStyleBackColor = true;
+            this.btnRaiz.Click += new System.EventHandler(this.btnRaiz_Click);
+            // 
+            // btnDividit
+            // 
+            this.btnDividit.BackgroundImage = global::CalculadoraTP.Properties.Resources.Divide2;
+            this.btnDividit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDividit.Location = new System.Drawing.Point(97, 81);
+            this.btnDividit.Name = "btnDividit";
+            this.btnDividit.Size = new System.Drawing.Size(33, 32);
+            this.btnDividit.TabIndex = 6;
+            this.btnDividit.UseVisualStyleBackColor = true;
+            this.btnDividit.Click += new System.EventHandler(this.btnDividit_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.BackgroundImage = global::CalculadoraTP.Properties.Resources.Multiply2;
+            this.btnMultiplicar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplicar.Location = new System.Drawing.Point(136, 80);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(34, 33);
+            this.btnMultiplicar.TabIndex = 5;
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // btnSubtrair
+            // 
+            this.btnSubtrair.BackgroundImage = global::CalculadoraTP.Properties.Resources.Minus2;
+            this.btnSubtrair.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubtrair.Location = new System.Drawing.Point(54, 80);
+            this.btnSubtrair.Name = "btnSubtrair";
+            this.btnSubtrair.Size = new System.Drawing.Size(33, 33);
+            this.btnSubtrair.TabIndex = 4;
+            this.btnSubtrair.Text = "-";
+            this.btnSubtrair.UseVisualStyleBackColor = true;
+            this.btnSubtrair.Click += new System.EventHandler(this.btnSubtrair_Click);
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.BackgroundImage = global::CalculadoraTP.Properties.Resources.Plus;
+            this.btnSomar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSomar.Location = new System.Drawing.Point(12, 80);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(34, 33);
+            this.btnSomar.TabIndex = 3;
+            this.btnSomar.Text = "+";
+            this.btnSomar.UseVisualStyleBackColor = true;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
+            // 
+            // frmCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(176, 187);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnConcatenar);
+            this.Controls.Add(this.btnEleva);
+            this.Controls.Add(this.btnRaiz);
+            this.Controls.Add(this.btnDividit);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnSubtrair);
+            this.Controls.Add(this.btnSomar);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.txtN1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmCalculadora";
+            this.Text = "CalculadoraTP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.Button btnSubtrair;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnDividit;
+        private System.Windows.Forms.Button btnRaiz;
+        private System.Windows.Forms.Button btnEleva;
+        private System.Windows.Forms.Button btnConcatenar;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
